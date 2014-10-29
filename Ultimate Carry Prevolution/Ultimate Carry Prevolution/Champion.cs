28399f19906cd5f259740734e3d8371405d76c5f
Lexxes <devLexxes@gmail.com>
2014-10-29 04:26:54 +0100

    Plugins

---
+++ Ultimate Carry Prevolution/Ultimate Carry Prevolution/Champion.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LeagueSharp;
+using LeagueSharp.Common;
+using LX_Orbwalker;
+
+namespace Ultimate_Carry_Prevolution
+{
+	class Champion
+	{
+		public Obj_AI_Hero MyHero = ObjectManager.Player;
+		public  IEnumerable<Obj_AI_Hero> AllHeros = ObjectManager.Get<Obj_AI_Hero>();
+		public  IEnumerable<Obj_AI_Hero> AllHerosFriend = ObjectManager.Get<Obj_AI_Hero>().Where(hero => hero.IsAlly);
+		public  IEnumerable<Obj_AI_Hero> AllHerosEnemy = ObjectManager.Get<Obj_AI_Hero>().Where(hero => hero.IsEnemy);
+	
+		public Spell Q;
+		public Spell W;
+		public Spell E;
+		public Spell R;
+
+		public static Menu Menu;
+		public LXOrbwalker Orbwalker;
+
+		public Champion()
+		{
+			LoadBasics();
+		}
+
+		private void LoadBasics()
+		{
+			Menu = new Menu("UC-Prevolution", MyHero.ChampionName + "_UCP", true);
+			//the Team
+			Menu.AddSubMenu(new Menu("UC-Team", "Credits"));
+			Menu.SubMenu("Credits").AddItem(new MenuItem("Lexxes", "Lexxes (Austria)"));
+			Menu.SubMenu("Credits").AddItem(new MenuItem("xSalice", "xSalice (US)"));
+			//PacketMenu
+			Menu.AddSubMenu(new Menu("Packet Setting", "Packets"));
+			Menu.SubMenu("Packets").AddItem(new MenuItem("usePackets", "Use Packets").SetValue(true));
+			//Orbwalker get changed to xSLxOrbwalker Soon
+			var orbwalkerMenu = new Menu("LX Orbwalker", "LX_Orbwalker");
+			LXOrbwalker.AddToMenu(orbwalkerMenu);
+			Menu.AddSubMenu(orbwalkerMenu);
+
+		}
+
+	}
+}
---
+++ Ultimate Carry Prevolution/Ultimate Carry Prevolution/Plugin/Aatrox.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp.Common;
+
+namespace Ultimate_Carry_Prevolution.Plugin
+{
+	class Aatrox : Champion
+	{
+		public Aatrox()
+		{
+			LoadMenu();
+		}
+
+		private void LoadMenu()
+		{
+			var champMenu = new Menu("Aatrox Plugin", "Aatrox");
+			{
+				var comboMenu = new Menu("Combo", "Combo");
+				comboMenu.AddItem(new MenuItem("useQ", "Use Q").SetValue(true));
+				champMenu.AddSubMenu(comboMenu);
+			}
+
+			Menu.AddSubMenu(champMenu);
+			Menu.AddToMainMenu();
+
+		}
+	}
+}
