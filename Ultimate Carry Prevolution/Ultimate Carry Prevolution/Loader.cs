f447594a5430530708140a52634b1750e79ffea6
Lexxes <devLexxes@gmail.com>
2014-10-29 04:25:18 +0100

    demo

--- Ultimate Carry Prevolution/Ultimate Carry Prevolution/Loader.cs
+++ Ultimate Carry Prevolution/Ultimate Carry Prevolution/Loader.cs
@@ -2,10 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using LeagueSharp;
+using LeagueSharp.Common;
 
 namespace Ultimate_Carry_Prevolution
 {
 	class Loader
 	{
+		public const double VersionNumber = 0.1;
+		public static Champion Champion;
+
+		public Loader()
+		{
+			Chat.WellCome();
+			LoadChampionPlugin();
+		}
+
+		private void LoadChampionPlugin()
+		{
+			try
+			{
+				// ReSharper disable once AssignNullToNotNullAttribute
+				var handle = Activator.CreateInstance(null, "Ultimate_Carry_Prevolution.Plugin." + ObjectManager.Player.ChampionName);
+				Champion = (Champion)handle.Unwrap();
+			}
+			// ReSharper disable once EmptyGeneralCatchClause
+			catch(Exception)
+			{
+			}
+		}
 	}
 }
