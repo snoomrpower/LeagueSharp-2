d61788686b13a27fcffb6f0d88c0de01fe95487b
Lexxes <devLexxes@gmail.com>
2014-11-01 01:20:24 +0100

    Add Aatrox

--- Ultimate Carry Prevolution/Ultimate Carry Prevolution/Champion.cs
+++ Ultimate Carry Prevolution/Ultimate Carry Prevolution/Champion.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Odbc;
 using System.Linq;
 using System.Text;
 using LeagueSharp;
 using LeagueSharp.Common;
-using LX_Orbwalker;
+using SharpDX;
+using xSLx_Orbwalker;
 
 namespace Ultimate_Carry_Prevolution
 {
@@ -21,11 +23,45 @@ namespace Ultimate_Carry_Prevolution
 		public Spell R;
 
 		public static Menu Menu;
-		public LXOrbwalker Orbwalker;
 
+		private List<DrawObject> _drawObjectlist = new List<DrawObject>();
 		public Champion()
 		{
 			LoadBasics();
+
+			Game.OnGameUpdate += OnGameUpdate;
+			Game.OnGameUpdate += OnGameUpdateModes;
+			Drawing.OnDraw += OnDraw;
+		}
+
+		private void OnGameUpdateModes(EventArgs args)
+		{
+
+			switch (xSLxOrbwalker.CurrentMode)
+			{
+				case xSLxOrbwalker.Mode.Combo:
+					OnCombo();
+					break;
+				case xSLxOrbwalker.Mode.Harass :
+					OnHarass();
+					break;
+				case xSLxOrbwalker.Mode.LaneClear :
+					OnLaneClear();
+					break;
+				case xSLxOrbwalker.Mode.LaneFreeze :
+					OnLaneFreeze();
+					break;
+				case xSLxOrbwalker.Mode.Lasthit :
+					OnLasthit();
+					break;
+				case xSLxOrbwalker.Mode.Flee :
+					OnFlee();
+					break;
+				case xSLxOrbwalker.Mode.None:
+					OnStandby();
+					break;
+			}
+			OnPassive();
 		}
 
 		private void LoadBasics()
@@ -39,13 +75,149 @@ namespace Ultimate_Carry_Prevolution
 			
 			//PacketMenu
 			Menu.AddSubMenu(new Menu("Packet Setting", "Packets"));
-			Menu.SubMenu("Packets").AddItem(new MenuItem("usePackets", "Use Packets").SetValue(true));
+			Menu.SubMenu("Packets").AddItem(new MenuItem("usePackets", "Use Packets").SetValue(false));
 			
 			//Orbwalker get changed to xSLxOrbwalker Soon
 			var orbwalkerMenu = new Menu("LX Orbwalker", "LX_Orbwalker");
-			LXOrbwalker.AddToMenu(orbwalkerMenu);
+			xSLxOrbwalker.AddToMenu(orbwalkerMenu);
 			Menu.AddSubMenu(orbwalkerMenu);
+		}
 
+		public bool UsePackets()
+		{
+			return Menu.Item("usePackets").GetValue<bool>();
+		}
+
+		public Obj_AI_Hero Cast_BasicSkillshot_Enemy(Spell spell, SimpleTs.DamageType prio = SimpleTs.DamageType.True, float extrarange = 0)
+		{
+			if(!spell.IsReady())
+				return null;
+			var target = SimpleTs.GetTarget(spell.Range + extrarange, prio);
+			if(target == null)
+				return null;
+			if(!target.IsValidTarget(spell.Range + extrarange) || spell.GetPrediction(target).Hitchance < HitChance.High)
+				return null;
+			spell.UpdateSourcePosition();
+			spell.Cast(target, UsePackets());
+			return target;
+		}
+	
+		public void Cast_BasicSkillshot_AOE_Farm(Spell spell, int extrawidth = 0)
+		{
+			if(!spell.IsReady())
+				return;
+			var minions = MinionManager.GetMinions(MyHero.ServerPosition, spell.Type == SkillshotType.SkillshotLine ? spell.Range : spell.Range + ((spell.Width + extrawidth) / 2), MinionTypes.All, MinionTeam.NotAlly);
+			if(minions.Count == 0)
+				return;
+			MinionManager.FarmLocation castPostion;
+
+			switch (spell.Type)
+			{
+				case SkillshotType.SkillshotCircle:
+					castPostion = MinionManager.GetBestCircularFarmLocation(minions.Select(minion => minion.ServerPosition.To2D()).ToList(), spell.Width + extrawidth, spell.Range);
+					break;
+				case SkillshotType.SkillshotLine:
+					castPostion = MinionManager.GetBestLineFarmLocation(
+						minions.Select(minion => minion.ServerPosition.To2D()).ToList(), spell.Width, spell.Range);
+					break;
+				default:
+					return;
+			}
+			spell.UpdateSourcePosition();
+			spell.Cast(castPostion.Position, UsePackets());
+		}
+
+		public static bool IsInsideEnemyTower(Vector3 position)
+		{
+			return ObjectManager.Get<Obj_AI_Turret>()
+									.Any(tower => tower.IsEnemy && tower.Health > 0 && tower.Position.Distance(position) < 775);
+		}
+
+		public void AddSpelltoMenu(Menu menu, string name, bool state,string alternativename = "")
+		{
+			if (alternativename == "")
+				alternativename = "Use " + menu;
+			menu.AddItem(new MenuItem(menu.Name + "_" + name.Replace(" ", "_"), alternativename).SetValue(state));
+		}
+
+		public bool IsSpellActive(string name)
+		{
+			try
+			{
+				return Menu.Item(xSLxOrbwalker.CurrentMode + "_" + name.Replace(" ", "_")).GetValue<bool>();
+			}
+			catch
+			{
+				return false;
+			}
+		}
+
+		public string GetSpellName(SpellSlot slot, Obj_AI_Hero unit = null)
+		{
+			return unit != null ? unit.Spellbook.GetSpell(slot).Name : MyHero.Spellbook.GetSpell(slot).Name;
+		}
+
+		public bool EnemysinRange(float range, int min = 1, Obj_AI_Hero unit = null)
+		{
+			if(unit == null)
+				unit = MyHero;
+			return min <= AllHerosEnemy.Count(hero => hero.Distance(unit) < range && hero.IsValidTarget());
+		}
+		public bool EnemysinRange(float range, int min, Vector3 pos)
+		{
+			return min <= AllHerosEnemy.Count(hero => hero.Position.Distance(pos) < range && hero.IsValidTarget() && !hero.IsDead);
+		}
+
+		public virtual void OnDraw(EventArgs args)
+		{
+			OnDraw();
+		}
+		public virtual void OnDraw()
+		{
+			// Virtual OnDraw
+		}
+		public virtual void OnStandby()
+		{
+			// Virtual OnStandby
+		}
+
+		public virtual void OnFlee()
+		{
+			// Virtual OnFlee
+		}
+
+		public virtual void OnLasthit()
+		{
+			// Virtual OnLasthit
+		}
+
+		public virtual void OnLaneFreeze()
+		{
+			// Virtual OnLaneFreeze
+		}
+
+		public virtual void OnLaneClear()
+		{
+			// Virtual OnLaneClear
+		}
+
+		public virtual void OnHarass()
+		{
+			// Virtual OnHarass
+		}
+
+		public virtual void OnCombo()
+		{
+			// Virtual OnCombo
+		}
+
+		public virtual void OnGameUpdate(EventArgs args)
+		{
+			// Virtual OnGameUpdate
+		}
+		public virtual void OnPassive()
+		{
+			// Virtual OnPassive
 		}
 
 	}
--- Ultimate Carry Prevolution/Ultimate Carry Prevolution/Plugin/Aatrox.cs
+++ Ultimate Carry Prevolution/Ultimate Carry Prevolution/Plugin/Aatrox.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
+using LeagueSharp;
 using LeagueSharp.Common;
+using xSLx_Orbwalker;
+using Color = System.Drawing.Color;
 
 namespace Ultimate_Carry_Prevolution.Plugin
 {
@@ -11,7 +10,21 @@ namespace Ultimate_Carry_Prevolution.Plugin
 	{
 		public Aatrox()
 		{
+			SetSpells();
 			LoadMenu();
+		}
+
+		private void SetSpells()
+		{
+			Q = new Spell(SpellSlot.Q, 650);
+			Q.SetSkillshot((float)0.27, 280, 1800, false, SkillshotType.SkillshotCircle);
+
+			W = new Spell(SpellSlot.W);
+
+			E = new Spell(SpellSlot.E, 1000);
+			E.SetSkillshot((float)0.27, 80, 1200, false, SkillshotType.SkillshotLine);
+
+			R = new Spell(SpellSlot.R, 300);			
 		}
 
 		private void LoadMenu()
@@ -19,13 +32,216 @@ namespace Ultimate_Carry_Prevolution.Plugin
 			var champMenu = new Menu("Aatrox Plugin", "Aatrox");
 			{
 				var comboMenu = new Menu("Combo", "Combo");
-				comboMenu.AddItem(new MenuItem("useQ", "Use Q").SetValue(true));
-				champMenu.AddSubMenu(comboMenu);
+				{
+					AddSpelltoMenu(comboMenu, "Q", true);
+					AddSpelltoMenu(comboMenu, "E", true);
+					comboMenu.AddItem(new MenuItem("Combo_useR_onAmount", "R on Enemys in Range").SetValue(new Slider(2, 5, 0)));
+					comboMenu.AddItem(new MenuItem("Combo_useR_onEnemyHealth", "R on Enemy %Health <").SetValue(new Slider(60, 100, 0)));
+					champMenu.AddSubMenu(comboMenu);
+				}
+				var harassMenu = new Menu("Harass", "Harass");
+				{
+					AddSpelltoMenu(harassMenu, "Q", true);
+					AddSpelltoMenu(harassMenu, "Q_Danger", true,"Q inside Dangerzone");
+					AddSpelltoMenu(harassMenu, "E", true);
+					champMenu.AddSubMenu(harassMenu);
+				}
+				var laneClearMenu = new Menu("LaneClear", "LaneClear");
+				{
+					AddSpelltoMenu(laneClearMenu, "Q", true);
+					AddSpelltoMenu(laneClearMenu, "E", true);
+					champMenu.AddSubMenu(laneClearMenu);
+				}
+				var fleeMenu = new Menu("Flee", "Flee");
+				{
+					AddSpelltoMenu(fleeMenu, "Q", true, "Use Q to Mouse");
+					AddSpelltoMenu(fleeMenu, "E", true, "Use E to slow Enemy");
+					champMenu.AddSubMenu(fleeMenu);
+				}
+				var miscMenu = new Menu("Misc", "Misc");
+				{
+					miscMenu.AddItem(new MenuItem("Misc_useW_Autoswitch", "Switch W Automatic").SetValue(true));
+					miscMenu.AddItem(new MenuItem("Misc_useW_Autoswitch_health", "Use E to slow Enemy").SetValue(new Slider(60, 100, 0)));
+					miscMenu.AddItem(new MenuItem("Misc_useW_Autoswitch_priorityhealth", "Heal Priority farming").SetValue(true));
+					champMenu.AddSubMenu(miscMenu);
+				}
+				var drawMenu = new Menu("Drawing", "Drawing");
+				{
+					drawMenu.AddItem(new MenuItem("Draw_Disabled", "Disable All").SetValue(false));
+					drawMenu.AddItem(new MenuItem("Draw_Q", "Draw Q").SetValue(true));
+					drawMenu.AddItem(new MenuItem("Draw_E", "Draw E").SetValue(true));
+					drawMenu.AddItem(new MenuItem("Draw_R", "Draw R").SetValue(true));
+					champMenu.AddSubMenu(drawMenu);
+				}
 			}
 
 			Menu.AddSubMenu(champMenu);
 			Menu.AddToMainMenu();
 
 		}
+
+		public override void OnDraw()
+		{
+			if(Menu.Item("Draw_Disabled").GetValue<bool>())
+			{
+				xSLxOrbwalker.DisableDrawing();
+				return;
+			}
+			xSLxOrbwalker.EnableDrawing();
+
+			if(Menu.Item("Draw_Q").GetValue<bool>())
+				if(Q.Level > 0)
+					Utility.DrawCircle(MyHero.Position, Q.Range, Q.IsReady() ? Color.Green : Color.Red);
+
+			if(Menu.Item("Draw_E").GetValue<bool>())
+				if(E.Level > 0)
+					Utility.DrawCircle(MyHero.Position, E.Range, E.IsReady() ? Color.Green : Color.Red);
+
+			if(Menu.Item("Draw_R").GetValue<bool>())
+				if(R.Level > 0)
+					Utility.DrawCircle(MyHero.Position, R.Range, R.IsReady() ? Color.Green : Color.Red);
+
+		}
+
+		public override void OnPassive()
+		{
+			AutomaticW();
+		}
+
+		public override void OnCombo()
+		{
+			if(IsSpellActive("E"))
+				Cast_E();
+			if(IsSpellActive("Q"))
+				Cast_Q();
+			Cast_R();
+		}
+
+		public override void OnHarass()
+		{
+			if(IsSpellActive("E"))
+				Cast_E();
+			if(IsSpellActive("Q"))
+				Cast_Q();
+		}
+
+		public override void OnLaneClear()
+		{
+			if(IsSpellActive("E"))
+				Cast_BasicSkillshot_AOE_Farm(Q);
+			if(IsSpellActive("Q"))
+				Cast_BasicSkillshot_AOE_Farm(E);
+		}
+
+		public override void OnFlee()
+		{
+			if(IsSpellActive("E"))
+				Cast_E();
+			if(IsSpellActive("Q"))
+				if(Game.CursorPos.Distance(MyHero.Position) > Q.Range && Q.IsReady())
+				{
+					Q.UpdateSourcePosition();
+					Q.Cast(Game.CursorPos, UsePackets());
+				}
+		}
+
+		private void AutomaticW()
+		{
+			if(!W.IsReady())
+				return;
+			if(!Menu.Item("useW_autoswitch").GetValue<bool>())
+				return;
+			if(xSLxOrbwalker.CurrentMode == xSLxOrbwalker.Mode.Combo)
+			{
+				if(MyHero.Health / MyHero.MaxHealth * 100 <
+					Menu.Item("useW_autoswitch_health").GetValue<Slider>().Value)
+				{
+					WtoHeal();
+					return;
+				}
+				WtoDamage();
+				return;
+			}
+			if(Menu.Item("useW_autoswitch").GetValue<bool>())
+			{
+				if(MyHero.Health / MyHero.MaxHealth * 100 < 95)
+					WtoHeal();
+				else
+					WtoDamage();
+			}
+			else
+			{
+				if(MyHero.Health / MyHero.MaxHealth * 100 <
+					Menu.Item("useW_autoswitch_health").GetValue<Slider>().Value)
+					WtoHeal();
+				else
+					WtoDamage();
+			}
+		}
+
+		private void WtoDamage()
+		{
+			if(GetSpellName(SpellSlot.W) == "aatroxw2" || !W.IsReady())
+				return;
+			W.Cast();
+		}
+
+		private void WtoHeal()
+		{
+			if(GetSpellName(SpellSlot.W) == "AatroxW" || !W.IsReady())
+				return;
+			W.Cast();
+		}
+
+		private void Cast_Q()
+		{
+			var target = SimpleTs.GetTarget(Q.Range + Q.Width / 2,SimpleTs.DamageType.Physical);
+			switch(xSLxOrbwalker.CurrentMode)
+			{
+				case xSLxOrbwalker.Mode.Combo:
+					if(target != null && !xSLxOrbwalker.InAutoAttackRange(target ))
+						Cast_BasicSkillshot_Enemy(Q, SimpleTs.DamageType.Physical);
+					break;
+				case xSLxOrbwalker.Mode.Harass:
+					if(target != null && !xSLxOrbwalker.InAutoAttackRange(target))
+						if(!IsSpellActive("Q_Danger"))
+						{
+							if(!IsInsideEnemyTower(Q.GetPrediction(target).CastPosition))
+								Cast_BasicSkillshot_Enemy(Q, SimpleTs.DamageType.Physical);
+						}
+						else
+							Cast_BasicSkillshot_Enemy(Q, SimpleTs.DamageType.Physical);
+
+					break;
+			}
+		}
+
+		private void Cast_E()
+		{
+			var target = SimpleTs.GetTarget(E.Range, SimpleTs.DamageType.Physical);
+			if(target != null)
+				Cast_BasicSkillshot_Enemy(E,SimpleTs.DamageType.Physical);
+		}
+
+		private void Cast_R()
+		{
+			if(!R.IsReady())
+				return;
+			if(Menu.Item("useR_Combo_Amount").GetValue<Slider>().Value > 0)
+				if(EnemysinRange(R.Range, Menu.Item("useR_Combo_Amount").GetValue<Slider>().Value))
+				{
+					R.Cast();
+					return;
+				}
+			if(Menu.Item("useR_Combo_EnemyHealh").GetValue<Slider>().Value <= 0)
+				return;
+			if(!AllHerosEnemy.Any(hero => hero.IsValidTarget(R.Range) &&
+											  hero.Health / hero.MaxHealth * 100 <
+											  Menu.Item("useR_Combo_EnemyHealh").GetValue<Slider>().Value))
+				return;
+			R.Cast();
+		}
+
+		
 	}
 }
